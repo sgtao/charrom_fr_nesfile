c2b2d275f1c784d8da7e12f4290a6b525f009625
Shogo Ogami <sg.tao.so@gmail.com>
2020-05-31 14:48:08 +0900

    temporary, try to canvas draw using an example of canvas module

--- charrom.js
+++ charrom.js
@@ -1,12 +1,37 @@
 // charrom.js
 const fs = require('fs');
 const { createCanvas, loadImage } = require('canvas')
-const canvas = createCanvas(200, 200)
-const ctx = canvas.getContext('2d')
 
 // check arguments
 // console.log(process);
 for (let i in process.argv) {
   console.log(`${i} : ${process.argv[i]}`);
 }
+
+// create canvas
+const canvas = createCanvas(200, 200)
+const ctx = canvas.getContext('2d')
+var distPath = 'charrom.png';
+
+// Write "Awesome!"
+ctx.font = '30px Impact'
+ctx.rotate(0.1)
+ctx.fillText('Awesome!', 50, 100)
+
+// Draw line under text
+var text = ctx.measureText('Awesome!')
+ctx.strokeStyle = 'rgba(0,0,0,0.5)'
+ctx.beginPath()
+ctx.lineTo(50, 102)
+ctx.lineTo(50 + text.width, 102)
+ctx.stroke()
+
+// output png file of cat with lime helmet
+loadImage('./lime-cat.jpg').then((image) => {
+  ctx.drawImage(image, 50, 0, 70, 70)
+
+  const data = canvas.toDataURL().split(',')[1];
+  const buffer = new Buffer(data, 'base64');
+  fs.writeFileSync(distPath, buffer);
+})
 
