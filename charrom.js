7a3abcf35908f4fd9cf3e090b8cd6c18c027842b
Shogo Ogami <sg.tao.so@gmail.com>
2020-05-31 18:06:50 +0900

    modify source style

--- charrom.js
+++ charrom.js
@@ -5,51 +5,70 @@ const fch = require('fetch');
 
 // check arguments
 let nesPath = 'samples/hello.nes';
-let distPath = 'charrom.png';
+let distPath = '--';
 for (let i in process.argv) {
-  console.log(`${i} : ${process.argv[i]}`);
+  // console.log(`${i} : ${process.argv[i]}`);
   if (i == 2) { nesPath  = process.argv[i]; }
   if (i == 3) { distPath = process.argv[i]; }
 }
 console.log(`input : ${nesPath}, output : ${distPath}`);
 
 // read nesfile
-let nes;
+let nesrom ;
 try {
-  nes = fs.readFileSync(nesPath);
+  nesrom = fs.readFileSync(nesPath);
 } catch (error) {
   throw new Error(error);
 }
-if ([].slice.call(nes, 0, 3).map(v => String.fromCharCode(v)).join('') !== 'NES') {
-  throw new Error(`${nesPath} is not an NES file.`);
+
+function toHEX(num) {
+  return num < 16 ? '0' + num.toString(16).toUpperCase() : 
+                    num.toString(16).toUpperCase(); 
 }
 
-// pullout charactor ROM to png file by reference bokuweb code
+// let header_record = Array.prototype.slice.call(nesrom, 0, 3);
+let hdr_record = [].slice.call(nesrom, 0, 15);
+let hdr_record_hex = '';
+for(let i in hdr_record){ hdr_record_hex += toHEX(hdr_record[i]) + ' '; }
+
+let hdr_record_str = hdr_record.slice(0,3).map(v => String.fromCharCode(v)).join('');
+const programROMPages = hdr_record[4];
+const characterROMPages = hdr_record[5];
 const NES_HEADER_SIZE = 0x0010;
-const PROGRAM_ROM_SIZE = 0x4000;
-const CHARACTOR_ROM_SIZE = 0x2000;
+const PROGRAM_ROM_SIZE = 0x4000;   // unit size is 16KB
+const CHARACTOR_ROM_SIZE = 0x2000; // unit size is 8KB
+
+const programROMstart  = NES_HEADER_SIZE;
+const charactorROMsart = programROMstart + programROMPages * PROGRAM_ROM_SIZE;
+const charactorROMend = charactorROMsart + characterROMPages * CHARACTOR_ROM_SIZE - 1;
+
+console.log('HDR(ADR[0:F]) : ' + hdr_record_hex);
+console.log(`HDR record    :  ${hdr_record_str}`);
+if (hdr_record_str !== 'NES') {
+  throw new Error(`${nesPath} is not an NES file.`);
+}
+console.log('progROM addr. : 0x', toHEX(programROMstart), ' - 0x', toHEX(charactorROMsart - 1));
+console.log('charROM addr. : 0x', toHEX(charactorROMsart), ' - 0x', toHEX(charactorROMend - 1));
+
+
+// pullout charactor ROM to png file by reference bokuweb code
 const DEFAULT_CANVAS_WIDTH = 800;
 const pixelSize = 2;
-
-const programROMPages = nes[4];
-const characterROMPages = nes[5];
 const spritesPerRow = DEFAULT_CANVAS_WIDTH / (8 * pixelSize);
 const spritesNum = CHARACTOR_ROM_SIZE * characterROMPages / 16;
 const rowNum = ~~(spritesNum / spritesPerRow) + 1;
-
 const height = rowNum * 8 * pixelSize;
+
 const canvas = createCanvas(DEFAULT_CANVAS_WIDTH, height);
 const ctx = canvas.getContext('2d');
-
 ctx.fillStyle = "rgb(0, 0, 0)";
 ctx.fillRect(0, 0, DEFAULT_CANVAS_WIDTH, height);
 
-const charactorROMsart = NES_HEADER_SIZE + programROMPages * PROGRAM_ROM_SIZE;
 const renderSprite = (spriteNum) => {
   for (let i = 0; i < 16; i++) {
     ctx.fillStyle = i > 7 ? "rgb(255, 255, 255)" : "rgb(128, 128, 128)";
     for (let j = 0; j < 8; j++) {
-      if (nes[charactorROMsart + spriteNum * 16 + i] & (0x80 >> j)) {
+      if (nesrom[charactorROMsart + spriteNum * 16 + i] & (0x80 >> j)) {
         const x = (j + (spriteNum % spritesPerRow) * 8) * pixelSize;
         const y = ((i % 8) + ~~(spriteNum / spritesPerRow) * 8) * pixelSize;
         ctx.fillRect(x, y, pixelSize, pixelSize);
@@ -64,4 +83,7 @@ for (let i = 0; i < spritesNum; i++) {
 
 const data = canvas.toDataURL().split(',')[1];
 const buffer = new Buffer(data, 'base64');
-fs.writeFileSync(distPath, buffer);
+if (distPath !== '--') 
+  fs.writeFileSync(distPath, buffer);
+else
+  console.log('file output is skipped.');
