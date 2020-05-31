ba6279468cd60f63757c4cd40a02ead50a60441b
Shogo Ogami <sg.tao.so@gmail.com>
2020-05-31 18:56:13 +0900

    add a converter to ascii art (, but comment out)

--- charrom.js
+++ charrom.js
@@ -21,16 +21,17 @@ try {
   throw new Error(error);
 }
 
-function toHEX(num) {
+const toHEX = (num) => {
   return num < 16 ? '0' + num.toString(16).toUpperCase() : 
                     num.toString(16).toUpperCase(); 
 }
-
+const strHEXarray = (buf) => {
+  let hex_str = '';
+  for (let i in buf) { hex_str += toHEX(buf[i]) + ' '; }
+  return hex_str;
+}
 // let header_record = Array.prototype.slice.call(nesrom, 0, 3);
 let hdr_record = [].slice.call(nesrom, 0, 15);
-let hdr_record_hex = '';
-for(let i in hdr_record){ hdr_record_hex += toHEX(hdr_record[i]) + ' '; }
-
 let hdr_record_str = hdr_record.slice(0,3).map(v => String.fromCharCode(v)).join('');
 const programROMPages = hdr_record[4];
 const characterROMPages = hdr_record[5];
@@ -42,7 +43,7 @@ const programROMstart  = NES_HEADER_SIZE;
 const charactorROMsart = programROMstart + programROMPages * PROGRAM_ROM_SIZE;
 const charactorROMend = charactorROMsart + characterROMPages * CHARACTOR_ROM_SIZE - 1;
 
-console.log('HDR(ADR[0:F]) : ' + hdr_record_hex);
+console.log('HDR(ADR[0:F]) : ' + strHEXarray(hdr_record));
 console.log(`HDR record    :  ${hdr_record_str}`);
 if (hdr_record_str !== 'NES') {
   throw new Error(`${nesPath} is not an NES file.`);
@@ -65,10 +66,43 @@ ctx.fillStyle = "rgb(0, 0, 0)";
 ctx.fillRect(0, 0, DEFAULT_CANVAS_WIDTH, height);
 
 const renderSprite = (spriteNum) => {
+  let sprite_addr   = charactorROMsart + spriteNum * 16 ;
+  let sprite_record = [].slice.call(nesrom, sprite_addr, sprite_addr + 15);
+  // console.log('sprite ', spriteNum, ' : addr = 0x' + toHEX(sprite_addr));
+  // console.log('  ' + strHEXarray(sprite_record));
+  // display ascii art.
+  /*
+  for (let i = 0; i < 8; i++) {
+    let column ='  ';
+    for (let j = 0; j < 8; j++) {
+      let bit_hi = (nesrom[sprite_addr + 8 + i] & (0x80 >> j)) >> (7-j);
+      let bit_lo = (nesrom[sprite_addr     + i] & (0x80 >> j)) >> (7-j);
+      switch ((bit_hi << 1) + bit_lo) {
+        case 0:
+          column += '  ';
+          break;;
+        case 1:
+          column += ' .';
+          break;;
+        case 2:
+          column += ' #';
+          break;;
+        case 3:
+          column += '##';
+          break;;
+        default:
+          column += '  ';
+          break;;
+      }
+    }
+    console.log(column);
+  }
+  */
+
   for (let i = 0; i < 16; i++) {
     ctx.fillStyle = i > 7 ? "rgb(255, 255, 255)" : "rgb(128, 128, 128)";
     for (let j = 0; j < 8; j++) {
-      if (nesrom[charactorROMsart + spriteNum * 16 + i] & (0x80 >> j)) {
+      if (nesrom[sprite_addr + i] & (0x80 >> j)) {
         const x = (j + (spriteNum % spritesPerRow) * 8) * pixelSize;
         const y = ((i % 8) + ~~(spriteNum / spritesPerRow) * 8) * pixelSize;
         ctx.fillRect(x, y, pixelSize, pixelSize);
